3bb13a15fdf7ee46c54d7da5e327b957f1d93387
Brandon Krigbaum <brandonbk@gmail.com>
2021-06-14 15:27:59 -0500

    Add index.js, core config

---
+++ tenants/all/config/core.js
@@ -0,0 +1,17 @@
+const emailX = require('./email-x');
+const nativeX = require('./native-x');
+
+const config = {
+  emailX,
+  nativeX,
+  dpm: {
+    emailx: {
+      enabled: true,
+    },
+  },
+  'aad-enewsletter-1': {
+    name: 'TBD',
+  },
+};
+
+module.exports = config;
