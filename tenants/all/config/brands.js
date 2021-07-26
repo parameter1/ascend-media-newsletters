ba84ebdd5639c5d6ee00c5c63145c152e9787910
Jessica Wade <jessica.wade@parameter1.com>
2021-07-26 12:12:25 -0500

    Create AUA Daily

--- tenants/all/config/brands.js
+++ tenants/all/config/brands.js
@@ -16,6 +16,29 @@ module.exports = {
     footer: {
       bgColor: '#1c7cd5',
       address: 'AAD Corporate Offices<br/>9500 W. Bryn Mawr<br/>Rosemont, IL 60018',
+      copyright: 'American Academy of Dermatology',
+    },
+  },
+  aua: {
+    headerImageSrc: '/files/base/ascend/hh/image/static/aua/aua-2021.png',
+    logoSrc: '',
+    socialMedia: {
+      imagePath: '/files/base/ascend/hh/image/static',
+      links: [
+        { provider: 'facebook', href: '', target: '_blank' },
+        { provider: 'instagram', href: '', target: '_blank' },
+        { provider: 'twitter', href: '', target: '_blank' },
+        { provider: 'linkedin', href: '', target: '_blank' },
+        { provider: 'youtube', href: '', target: '_blank' },
+      ],
+    },
+    ctaLinkStyle: {
+      color: '#22919c',
+    },
+    footer: {
+      bgColor: '#22919c',
+      address: 'AUA Corporate Offices<br/>1000 Corporate Boulevard<br/>Linthicum, MD 21090',
+      copyright: 'American Urological Association',
     },
   },
 };
