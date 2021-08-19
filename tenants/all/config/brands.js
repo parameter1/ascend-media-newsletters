93474d4e3dbc8b5c57e69c27987d753a7996029c
Jessica Wade <jessica.wade@parameter1.com>
2021-08-19 14:48:43 -0500

    Initial setup for acep brands.js

--- tenants/all/config/brands.js
+++ tenants/all/config/brands.js
@@ -69,4 +69,26 @@ module.exports = {
       ],
     },
   },
+  acep: {
+    logoSrc: '',
+    headerImageSrc: '',
+    socialMedia: {
+      imagePath: '',
+      links: [
+        { provider: 'facebook', href: '', target: '_blank' },
+        { provider: 'instagram', href: '', target: '_blank' },
+        { provider: 'twitter', href: '', target: '_blank' },
+      ],
+    },
+    ctaLinkStyle: {
+      color: '',
+    },
+    footer: {
+      bgColor: '',
+      address: '',
+      copyright: '',
+      unsubscribe: 'If you no longer wish to receive industry promotional emails for the , ',
+      vendorTagline: '',
+    },
+  },
 };
