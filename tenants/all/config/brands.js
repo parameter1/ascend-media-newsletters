d390afecc5e0bea1d92f7dbf26e20af19ff28053
Jessica Wade <jessica.wade@parameter1.com>
2021-08-31 13:04:10 -0500

    Create initial brands.js setup for asa

--- tenants/all/config/brands.js
+++ tenants/all/config/brands.js
@@ -134,4 +134,26 @@ module.exports = {
       vendorTagline: 'Ascend Media is an official advertising vendor for the ',
     },
   },
+  asa: {
+    logoSrc: '',
+    headerImageSrc: '',
+    socialMed: {
+      imagePath: '',
+      links: [
+        { provider: 'facebook', href: '', target: '_blank' },
+        { provider: 'instagram', href: '', target: '_blank' },
+        { provider: 'twitter', href: '', target: '_blank' },
+      ],
+    },
+    ctaLinkStyle: {
+      color: '',
+    },
+    footer: {
+      bgColor: '',
+      address: '',
+      copyright: '',
+      unsubscribe: 'If you no longer wish to receive industry promotional emails for the , ',
+      vendorTagline: 'Ascend Media is an official advertising vendor for the ',
+    },
+  },
 };
