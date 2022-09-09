74cb3034f10aea37f9e36a66add934c7f34f3310
Jessica Wade <jessica.wade@parameter1.com>
2022-09-09 11:13:05 -0500

    Update button link ASA 2022

--- tenants/all/config/core.js
+++ tenants/all/config/core.js
@@ -306,7 +306,7 @@ const config = {
       links: [
         { label: 'My Meeting App', href: '#', target: '_blank' },
         { label: 'Program', href: 'https://www.abstractsonline.com/pp8/#!/10552', target: '_blank' },
-        { label: 'Exhibit Hall', href: '#', target: '_blank' },
+        { label: 'Exhibit Hall', href: 'https://www.asahq.org/annualmeeting/network/exhibit-hall', target: '_blank' },
         { label: 'Governance Resources', href: 'https://www.asahq.org/annualmeeting/governance', target: '_blank' },
       ],
     },
