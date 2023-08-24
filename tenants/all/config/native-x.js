99ab0320c5bfbba9681cc4b98b004f07a5509bbe
Brandon Krigbaum <brandonbk@gmail.com>
2023-08-24 09:32:24 -0500

    Update the native-x uri

--- tenants/all/config/native-x.js
+++ tenants/all/config/native-x.js
@@ -1,5 +1,5 @@
 module.exports = {
-  uri: 'https://ascend.native-x.parameter1.com',
+  uri: 'https://ascend.native-x.parameter1.com/email-placement',
   placements: {
     'aad-enewsletter-1': {
       'native-slot-1': '60c9e3742134870001cf88fc',
